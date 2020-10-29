6442037caa130cd95f7005d118c0594928ffdbf4
Francisco Rodriguez <francisco37.fr@gmail.com>
2020-10-28 19:49:59 -0700

    Part #2 Complete

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,17 +6,40 @@
 //
 
 import UIKit
+import Parse
+
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        // --- Copy this only
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "hfBP4C11GhdwrzQHkQ2feyNbHsCHmbib4hY7cn2w"
+                $0.clientKey = "8JYuX2ti865uXg3emoFVWD8LSxyAI90RUi2wE663"
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
+       // if PFUser.current() != nil {
+          //  let main = UIStoryboard(name: "Main", bundle: nil)
+           // let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
+            
+            //window?.rootViewController = feedNavigationController
+       // }
+        
+    
+
         return true
     }
-
+    
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
